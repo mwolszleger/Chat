4319888113a7fed2473cd062596b1e3fb631f408
AllBundy <securaplayer@o2.pl>
2016-11-10 20:53:10 +0100

    -Zmieniono nazwe klasy DatabaseManagment na DatabaseManagement (...gEm...) -Dodano do klasy CLient eventy MessageReceivedEvent, ktory ma za zadanie poinformowac zainteresowanych (serwer) o tym, iz obiekt klienta wyslal wiadomosc oraz event DisconnectedEvent majacy za zadanie poinformowac zainteresowanych (serwer) o tym, iz dany klient sie rozlaczyl -Dodano klase EventArgsWithContent reprezentujaca EventArgs z dodatkowym polem typu String -Poprawiono metode ReceiveCallback w klasie Client by infomrowala serwer o odbiorze wiadomosci lub rozlaczeniu sie poprzez eventy -Zaimplementowano statyczna metode tworzaca obiekt String z tablicy bajtow oraz tablice bajtow z obiektu typu String -Zaimplementowano obsluge wystapienia eventow otrzymania wiadomosci oraz informacji odnosnie rozlaczenia sie przez klienta w klasie Connection -Poprawiono metode AcceptCallback o czekanie na informacje odnosnie rozkazu loginu/rejestracji i dalszej obslugi klienta -Zaimplementowano mechanizm operowania na liscie klientow -Zaimplementowano wstepny mechanizm konwersji rozkazu do konkretnego polecenia dla serwera, wysylania wiadomosci do jednej oraz do wielu osob naraz -Refaktoryzacja kodu klasy ServerChat

--- Serwer/Serwer/Connection.cs
+++ Serwer/Serwer/Connection.cs
@@ -8,25 +8,40 @@ using System.Net;
 
 namespace Serwer
 {
-
     class Connection
     {
         #region Private Variables
         private Socket _serverSocket;
         private int _serverPort = 1024;
-        //private List<Client> _clientSockets;
-        private string[] nameTab = { "oko", "autor", "myszka miki", "zdzisiek", "kubek", "scooby doo", "cokolwiek", "foo", "test", "odbiorca" };
+        private byte[] _byteArray;
+        private List<Client> _clientSockets;
         #endregion
 
         public Connection()
         {
             _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
             _serverSocket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), _serverPort));
-           // _clientSockets = new List<Client>();
+            _clientSockets = new List<Client>();
+            _byteArray = new byte[1024];
             Console.WriteLine("Server wystartowal...");
         }
 
+        public void HandleMessageReceivedEvent(object sender, EventArgsWithContent args)
+        {
+            if (args.Content.Length != 0)
+            {
+                HandleOrder(args.Content, ((Client)sender).ClientSocket);
+                Console.WriteLine(((Client)sender).Name + " wyslal wiadomosc: " + args.Content);
+            }
+        }
+
+        public void HandleDisconnectedEvent(object sender, EventArgsWithContent args)
+        {
+            DisconnectClient(_clientSockets[_clientSockets.FindIndex(a => a.Name == args.Content)]);
+        }
+
         #region Methods
+
         #region Server Methods
         public void Listen()
         {
@@ -39,45 +54,201 @@ namespace Serwer
             _serverSocket.BeginAccept(AcceptCallback, _serverSocket);
         }
 
-
         private void AcceptCallback(IAsyncResult result)
         {
             Socket server = (Socket)result.AsyncState;
-            Random foo = new Random();
-            String oko = nameTab[foo.Next(0, nameTab.Length - 1)];
-            Client client = new Client(oko, server.EndAccept(result));
-            Console.WriteLine("Polaczyl sie " +oko);
+            Socket clientSocket = server.EndAccept(result);
 
+            //wait for login/register order      
+            clientSocket.Receive(_byteArray);
+
+            //handle login/register order, creates new client
+            Client client = this.HandleOrder(CreateStringFromByteArray(_byteArray), clientSocket);
+
+            //server subscribes to client's MessageReceivedEvent
+            client.MessageReceivedEvent += HandleMessageReceivedEvent;
+            client.DisconnectedEvent += HandleDisconnectedEvent;
 
             client.BeginReceive();
-
-            server.BeginAccept(AcceptCallback, server); // <- continue accepting connections
+            server.BeginAccept(AcceptCallback, server);
         }
 
         #endregion
 
         #region Handling Client Methods
 
-        /*public void AddClient(Client client)
+        private void AddClient(Client client)
         {
             _clientSockets.Add(client);
-            //Console.WriteLine("Polaczyl sie klient: " + client.Item1);
+            Console.WriteLine("Polaczyl sie " + client.Name);
+            //inform all clients, that someone connected
+            //check if there are any awaiting message for this client
         }
 
-        public Client GetClient()
+        private Client GetClient(string name)
         {
-            return _clientSockets[0];
+            return _clientSockets[_clientSockets.FindIndex(a => a.Name == name)];
         }
 
-        public void DisconnectClient(Client client)
+        private void DisconnectClient(Client client)
         {
             _clientSockets.Remove(client);
-            //Console.WriteLine("Klient sie rozlaczyl: " + client.Item1);
-        }*/
+            //inform all clients that someone disconnected
+        }
 
         #endregion
 
+        #region Private Methods
+
+        private Client HandleOrder(string order, Socket client)
+        {
+            String[] splitedOrder = order.Split(':');
+
+            switch (splitedOrder[0])
+            {
+                case "sendMsg":
+                    if (SendMsg(order))
+                        Console.WriteLine("Udalo sie wyslac wiadomosc");
+                    else
+                        Console.WriteLine("Nie udalo sie wyslac wiadomosci");
+                    break;
+
+                case "broadcast":
+                    if (Broadcast(order))
+                        Console.WriteLine("Udalo sie wyslac wiadomosci do wszystkich osob");
+                    else
+                        Console.WriteLine("Nie udalo sie wyslac wiadomosci");
+                    break;
+
+                case "login":
+                    //check if correct,
+                    Client cl = new Client(splitedOrder[1], client);
+                    this.AddClient(cl);
+                    return cl;
+                //inform about success and inform all clients
+                //login:nickname:shrtpass
+                //normal split
+
+                case "register":
+                    //add record to database and call login
+                    //register:nickname:shrtpass
+                    //normal split
+                    break;
+
+                default:
+                    break;
+            }
+            return new Client();
+        }
+
+        private bool SendMsg(string order)
+        {
+            String[] commands = SplitOrder(order);
+            //[0] = order
+            //[1] = author
+            //[2] = receiver
+            //[3] = content
+            if (_clientSockets.FindIndex(a => a.Name == commands[2]) != -1)
+            {
+                //sends message as order, i.e. sendMsg:authorOfMessage:receiverOfMessage:content
+                //if you want to send msg to yourself, create order as "sendMsg:author:author:conent"
+                GetClient(commands[2]).ClientSocket.Send(CreateByteArray(CreateOrder(commands)));
+                return true;
+            }
+            else
+            {
+                //write to database
+            }
+            return false;
+        }
+
+        private bool Broadcast(string order)
+        {
+            String[] commands = SplitOrder(order);
+            //[0] = order
+            //[1] = author
+            //[2] = receiverS
+            //[3] = content
+            String[] receivers = commands[2].Split(',');
+            bool success = false;
+
+            foreach (string item in receivers)
+            {
+                if (_clientSockets.FindIndex(a => a.Name == item) != -1)
+                {
+                    //sends message as order, i.e. broadcast:authorOfMessage:receiversOfMessage:content
+                    GetClient(item).ClientSocket.Send(CreateByteArray(CreateOrder(commands)));
+                    success = true;
+                }
+                else
+                {
+                    //przeslij do bazy i wyslij, gdy sie zaloguje
+                    success = true;
+                }
+            }
+            if (success)
+                return true;
+            return false;
+        }
+
+        //TO DO
+        /*  
+        private bool Login(string order)
+        {
+            return true;
+        }
+
+        private bool Register(string order)
+        {
+            //create login order
+            //call Login
+            return true;
+        }
+         private bool CheckAndSendOldMessages(Client client)
+        {
+            //get message from database and send to the client
+            return true;
+        }    
+         */
+
+        #region Methods to simplify
+        private static string CreateStringFromByteArray(byte[] byteArr)
+        {
+            return ASCIIEncoding.ASCII.GetString(byteArr);
+        }
+
+        private static byte[] CreateByteArray(string str)
+        {
+            return System.Text.Encoding.ASCII.GetBytes(str);
+        }
+
+        private String[] SplitOrder(string order)
+        {
+            String[] temp = new String[4];
+            int[] indexArr = new int[3];
+            int i = -1, j = 0;
+
+            while (j < 3 || i == order.Length)
+            {
+                if (order[++i] == ':')
+                    indexArr[j++] = ++i;
+            }
+            temp[0] = order.Substring(0, indexArr[0] - 1);
+            temp[1] = order.Substring(indexArr[0], indexArr[1] - indexArr[0] - 1);
+            temp[2] = order.Substring(indexArr[1], indexArr[2] - indexArr[1] - 1);
+            temp[3] = order.Substring(indexArr[2], order.IndexOf('\0') - indexArr[2]);
+            return temp;
+        }
+
+        private String CreateOrder(String[] commands)
+        {
+            return commands[0] + ":" + commands[1] + ":" + commands[2] + ":" + commands[3];
+        }
 
         #endregion
-    }
-}
+
+        #endregion
+
+        #endregion
+    }//end Connection class
+}//end namespace
--- Serwer/Serwer/ServerChat.cs
+++ Serwer/Serwer/ServerChat.cs
@@ -12,70 +12,13 @@ using System.Net;
 namespace Serwer
 {
     class Program
-    {    
-        static byte[] recBuffer = new byte[256];
-
+    {
         static void Main(string[] args)
         {
             Connection server = new Connection();
-            Thread th;
-
-            //while (true)
-            //{              
-                server.Listen();
-                server.BeginAccept();
-                //server.AddClient(GetStringSocketPair(server.Accept()));
-                //server.SendMessage("foo", "author", "foo");
-                //th = new Thread(() => writing(GetSocketFromPair(server.GetClient())));
-                //th.Start();
-           // }     
+            server.Listen();
+            server.BeginAccept();
             Console.Read();
         }
-
-        public static Tuple<string, Socket> GetStringSocketPair(Socket socket)
-        {
-            return new Tuple<string, Socket>("author", socket);
-        }
-
-        public static Socket GetSocketFromPair(Tuple<string, Socket> foo)
-        {
-            return foo.Item2;
-        }
-
-        public static void writing(Socket serv)
-        {
-            while (true)
-            {
-
-                bool part1 = serv.Poll(1000, SelectMode.SelectRead);
-                bool part2 = (serv.Available == 0);
-                bool part3;               
-                if (part1 && part2)
-                    part3 = false;
-                else
-                    part3 = true;
-
-
-                if (part3 == true)
-                {
-                    serv.Receive(recBuffer);
-                    //Console.WriteLine(serv.GetHashCode());
-                    //Console.WriteLine(ASCIIEncoding.ASCII.GetString(recBuffer));
-
-                    //recBuffer = System.Text.Encoding.ASCII.GetBytes("Bardzo dlugi tekst i zastanawiam sie jak bardzo dlugi on moze byc by normalnie dalej to przeszlo ciekawe co nie no to sprawdzimy hehehehehehehehe okoko nienienie");
-                    //serv.Send(recBuffer);
-                    //recBuffer = System.Text.Encoding.ASCII.GetBytes("Receiver");
-                    //serv.Send(recBuffer);
-
-
-                    //Array.Clear(recBuffer, 0, recBuffer.Length);
-                }
-                else break;
-            }
-            Console.WriteLine("okoko");
-        }
-
-
-
     }//end Main class
 }//end namespace
